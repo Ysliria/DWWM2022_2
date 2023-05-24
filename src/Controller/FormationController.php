87c2037beb2f4495778cbd31beb1c166d73081d0
Mickaël AUGER <auger.mickael37@gmail.com>
2023-05-24 11:38:40 +0200

    [PART2] Page détail et redirection

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -10,30 +10,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/formation', name: 'formation_')]
 class FormationController extends AbstractController
 {
-    #[Route('/formation', name: 'formation_index')]
-    public function index(FormationRepository$formationRepository): Response
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(FormationRepository $formationRepository): Response
     {
-        $formations = $formationRepository->findAll();
-
         return $this->render('formation/index.html.twig', [
-            'formations' => $formations,
+            'formations' => $formationRepository->findAll(),
         ]);
     }
 
-    #[Route('/formation/add', name: 'formation_add', methods: ['POST', 'GET'])]
+    #[Route('/{formation}/detail', name: 'show')]
+    public function show(Formation $formation)
+    {
+        return $this->render('formation/show.html.twig', [
+            'formation' => $formation
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'add', methods: ['POST', 'GET'])]
     public function add(Request $request, FormationRepository $formationRepository): Response
     {
-        $formation = new Formation();
-        $formationAdd   = $this->createForm(FormationType::class, $formation);
+        $formation    = new Formation();
+        $formationAdd = $this->createForm(FormationType::class, $formation);
 
         $formationAdd->handleRequest($request);
 
         if ($formationAdd->isSubmitted() && $formationAdd->isValid()) {
             $formationRepository->save($formation, true);
 
-            // @todo : ajouter une redirection vers l'index après la création
+            return $this->redirectToRoute(
+                'formation_show',
+                ['formation' => $formation->getId()],
+                Response::HTTP_SEE_OTHER
+            );
         }
 
         return $this->render('formation/add.html.twig', [
