2aa6858a1ac9bde107f2e126c490a154d35213a2
Mickaël AUGER <auger.mickael37@gmail.com>
2023-05-26 16:39:06 +0200

    [BONUS] Gestions des filières

--- src/Controller/FiliereController.php
+++ src/Controller/FiliereController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_ADMIN')]
 class FiliereController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(FiliereRepository $filiereRepository): Response
     {
         return $this->render('filiere/index.html.twig', [
@@ -23,7 +23,15 @@ class FiliereController extends AbstractController
         ]);
     }
 
-    #[Route('/ajouter', name: 'add')]
+    #[Route('/{filiere}/detail', name: 'show', methods: ['GET'])]
+    public function detail(Filiere$filiere): Response
+    {
+        return $this->render('filiere/show.html.twig', [
+            'filiere' => $filiere
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'add', methods: ['GET', 'POST'])]
     public function add(Request $request, FiliereRepository $filiereRepository): Response
     {
         $filiere = new Filiere();
@@ -34,7 +42,7 @@ class FiliereController extends AbstractController
         if ($filiereForm->isSubmitted() && $filiereForm->isValid()) {
             $filiereRepository->save($filiere, true);
 
-            $this->addFlash('success', 'La filière a bien été ajouté !');
+            $this->addFlash('success', 'La filière a bien été ajoutée !');
 
             return $this->redirectToRoute('filiere_index', [], Response::HTTP_SEE_OTHER);
         }
@@ -42,5 +50,39 @@ class FiliereController extends AbstractController
         return $this->render('filiere/add.html.twig', [
             'filiere_form' => $filiereForm
         ]);
+    }
+
+    #[Route('/{filiere}/modifier', name: 'update', methods: ['GET', 'POST'])]
+    public function update(Filiere $filiere, FiliereRepository $filiereRepository, Request $request): Response
+    {
+        $filiereForm = $this->createForm(FiliereType::class, $filiere);
+
+        $filiereForm->handleRequest($request);
+
+        if ($filiereForm->isSubmitted() && $filiereForm->isValid()) {
+            $filiereRepository->save($filiere, true);
+
+            $this->addFlash('success', 'La filière ' . $filiere->getName() . ' a bien été modifiée !');
+
+            return $this->redirectToRoute('filiere_show', ['filiere' => $filiere->getId()], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('filiere/add.html.twig', [
+            'filiere_form' => $filiereForm
+        ]);
+    }
+
+    #[Route('/{filiere}/supprimer', name: 'delete', methods: ['POST'])]
+    public function delete(Filiere $filiere, FiliereRepository $filiereRepository, Request $request): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $filiere->getId(), $request->request->get('_token'))) {
+            $filiereRepository->remove($filiere, true);
+
+            $this->addFlash('success', 'La filière " ' . $filiere->getName() . ' " a bien été supprimée !');
+        } else {
+            $this->addFlash('warning', 'Une erreur est survenue !');
+        }
+
+        return $this->redirectToRoute('filiere_index');
     }
 }
